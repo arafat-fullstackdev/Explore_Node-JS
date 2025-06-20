5079961619707be3bc8e7ea609af73e2f3bf4db0
Arafat Patwary <farabipsk@gmail.com>
2025-06-20 22:33:11 +0600

    Modify esm

---
+++ esm/file-2.js
@@ -0,0 +1,9 @@
+const a = 10;
+const add = (a,b) => a +b;
+const b = 20;
+
+export{
+    a,
+    b,
+    add
+};
---
+++ esm/file-3.js
@@ -0,0 +1,5 @@
+const {a,b, add} = require('./file-1');
+ const {a:a1,b:b1, add:add1} = require('./file-1');
+
+console.log({a,b});
+console.log({a1,b1,add1});
