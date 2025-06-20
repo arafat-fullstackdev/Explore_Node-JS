4158b581bb3d668b89aa1c190f9407b6255d99c3
Arafat Patwary <farabipsk@gmail.com>
2025-06-20 22:25:45 +0600

    Done node b

--- ToDo_App/server.js
+++ ToDo_App/server.js
@@ -1,10 +1,17 @@
 const http = require("http");
+const { stringify } = require("querystring");
 
 const server = http.createServer((req, res)=>{
         console.log({req,res});
 
        res.end('Welcome to ToDO App!');
 });
+
+const animal ={
+    name: 'Elephant',
+    nickName: 'EloneTusk'
+};
+console.log(typeof stringify(animal));
 
 server.listen(5000, '127.0.0.1', ()=>{
     console.log('Server is Running on port 5000');
