739f816b9bcede5e8c82126ebc9884f24801de5e
Arafat Patwary <farabipsk@gmail.com>
2025-06-24 10:51:42 +0600

    export module

--- esm/file-3.js
+++ esm/file-3.js
@@ -1,5 +1,8 @@
-const {a,b, add} = require('./file-1');
+// const var1 = require('./file-1');
+// const {a,b, add} = require('./file-1');
  const {a:a1,b:b1, add:add1} = require('./file-1');
 
-console.log({a,b});
-console.log({a1,b1,add1});
+// console.log({a1,b1});
+// console.log(add1(22,44));
+// console.log(var1.a2)
+// console.log(var1.b2)
