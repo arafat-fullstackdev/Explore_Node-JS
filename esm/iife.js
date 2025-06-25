1c72f5e0067a3a5bb417d64b5b10e4a64256707c
Arafat Patwary <farabipsk@gmail.com>
2025-06-25 10:54:45 +0600

    Explore IIFE

---
+++ esm/iife.js
@@ -0,0 +1,11 @@
+// ((name)=>{
+//     console.log(`Learning ${name}`)
+// })('Node') 
+let a = 12;
+// console.log(global)
+// console.log(module)
+// console.log(__filename)
+// console.log(__dirname)
+
+const os = require('os')
+console.log(os.cpus());
