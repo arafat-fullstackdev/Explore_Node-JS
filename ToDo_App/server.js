f31a4136ffe5ec2193251973923885b586e9c44b
Arafat Patwary <farabipsk@gmail.com>
2025-07-02 11:54:40 +0600

    Modify ToDO App server

--- ToDo_App/server.js
+++ ToDo_App/server.js
@@ -1,18 +1,29 @@
-const http = require("http");
-const { stringify } = require("querystring");
+// const http = require("http");
+// const { stringify } = require("querystring");
 
-const server = http.createServer((req, res)=>{
-        console.log({req,res});
+// const server = http.createServer((req, res)=>{
+//         console.log({req,res});
 
-       res.end('Welcome to ToDO App!');
-});
+//        res.end('Welcome to ToDO App!');
+// });
 
-const animal ={
-    name: 'Elephant',
-    nickName: 'EloneTusk'
-};
-console.log(typeof stringify(animal));
+// const animal ={
+//     name: 'Elephant',
+//     nickName: 'EloneTusk'
+// };
+// console.log(typeof stringify(animal));
 
-server.listen(5000, '127.0.0.1', ()=>{
-    console.log('Server is Running on port 5000');
-});
+// server.listen(5000, '127.0.0.1', ()=>{
+//     console.log('Server is Running on port 5000');
+// }); 
+
+const http = require('http')
+
+const server = http.createServer((req,res) =>{
+    console.log({req,res});
+    res.end('Welcome to ToDo App');
+})
+
+server.listen(4042, '127.0.0.1', ()=>{
+    console.log('New Server')
+}) 
